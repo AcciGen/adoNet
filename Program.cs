a6aa6866b522663211aa9f7b3b1d038dc814d476
Nuriddin Asrorov <denrov13@gmail.com>
2024-02-09 12:27:41 +0500

    some changes

--- Program.cs
+++ Program.cs
@@ -6,37 +6,21 @@ namespace adoNetTest
     {
         static void Main(string[] args)
         {
-            const string CONNECTIONSTRING = "Server=localhost;Port=5432;Database=Demo;User Id=postgres;Password=135;";
-            
-            NpgsqlConnection connection = new NpgsqlConnection(CONNECTIONSTRING);
-
-            connection.Open();
-
-            //string query = "Create table if not exists demo_table(id serial, theme varchar(40), start_date date, students_count int);";
-            //string insertQuery = "insert into demo_table(theme, start_date";
+            \
+            //Select
+            //string query = "select * from demo_table";
 
             //NpgsqlCommand command = new NpgsqlCommand(query, connection);
-            //command.ExecuteNonQuery();
 
-            //DemoDB table = new DemoDB();
-            //table .mav
+            //NpgsqlDataReader reader = command.ExecuteReader();
 
-            string query = "select * from demo_table";
-
-            NpgsqlCommand command = new NpgsqlCommand(query, connection);
-
-            NpgsqlDataReader reader = command.ExecuteReader();
-
-            while (reader.Read())
-            {
-                Console.WriteLine("{0}, {1}, {2}, {3}", reader.GetInt32(0),
-                    reader.GetString(1), reader.GetDateTime(2), reader.GetInt32(3));
-            }
-            
-            // logic
-            //Console.WriteLine("Created table!");
+            //while (reader.Read())
+            //{
+            //    Console.WriteLine($"{reader[0]}, {reader[1]}, {reader[2]}, {reader[3]}");
+            //}
 
             connection.Close();
         }
+
     }
 }
---
+++ crud.cs
@@ -0,0 +1,54 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adoNetTest
+{
+    public class CRUD
+    {
+        private readonly string CONNECTIONSTRING = "Server=localhost;Port=5432;Database=Demo;User Id=postgres;Password=135;";
+        string? query;
+
+        public string Create(string tableName, params[] values)
+        {
+            NpgsqlConnection connection = new NpgsqlConnection(CONNECTIONSTRING);
+
+            connection.Open();
+            //logic
+            query = "Create table if not exists demo_table(id serial, theme varchar(40), start_date date, students_count int);";
+
+            NpgsqlCommand command = new NpgsqlCommand(query, connection);
+            command.ExecuteNonQuery();
+
+            connection.Close();
+
+            return $"{tableName} was created successfully!";
+        }
+
+        public void Select(string tableName)
+        {
+            NpgsqlConnection connection = new NpgsqlConnection(CONNECTIONSTRING);
+            connection.Open();
+
+            query = $"select * from {tableName}";
+
+            NpgsqlCommand command = new NpgsqlCommand(query, connection);
+
+            NpgsqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.Write($"{reader[i]} ");
+                }
+            }
+            connection.Close();
+        }
+
+        public 
+    }
+}
