9ebfbde72b1fee13f07e0499eeb2d1115068135c
Nuriddin Asrorov <denrov13@gmail.com>
2024-02-10 22:26:50 +0500

    Some changes

---
+++ CRUD.cs
@@ -0,0 +1,55 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adoNetTest
+{
+    public class CRUD
+    {
+        private readonly string CONNECTIONSTRING = "Server=localhost;Port=5432;Database=Assignment;User Id=postgres;Password=135;";
+        string? query;
+
+        public string Create(string tableName, string fullName, string email, string password, string phoneNumber)
+        {
+            NpgsqlConnection connection = new NpgsqlConnection(CONNECTIONSTRING);
+            connection.Open();
+            
+            query = $"Create table if not exists {tableName}(id serial, fullName varchar(50), email varchar(100), password varchar(100), phoneNumber varchar(50));";
+
+            NpgsqlCommand command = new NpgsqlCommand(query, connection);
+            
+            command.ExecuteNonQuery();
+
+            connection.Close();
+            return $"{tableName} was created successfully!";
+        }
+
+        public void GetAll(string tableName)
+        {
+            NpgsqlConnection connection = new NpgsqlConnection(CONNECTIONSTRING);
+            connection.Open();
+
+            query = $"select * from {tableName}";
+
+            NpgsqlCommand command = new NpgsqlCommand(query, connection);
+
+            NpgsqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.Write($"{reader[i]} ");
+                }
+                Console.WriteLine();
+            }
+
+            connection.Close();
+        }
+
+        public  
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,20 +6,7 @@ namespace adoNetTest
     {
         static void Main(string[] args)
         {
-            \
-            //Select
-            //string query = "select * from demo_table";
-
-            //NpgsqlCommand command = new NpgsqlCommand(query, connection);
-
-            //NpgsqlDataReader reader = command.ExecuteReader();
-
-            //while (reader.Read())
-            //{
-            //    Console.WriteLine($"{reader[0]}, {reader[1]}, {reader[2]}, {reader[3]}");
-            //}
-
-            connection.Close();
+            CRUD.Create();
         }
 
     }
