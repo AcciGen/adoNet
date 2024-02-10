d67347fd84ae0cfecd8a68aaba7a35aea008814b
Nuriddin Asrorov <denrov13@gmail.com>
2024-02-10 22:53:32 +0500

    Changes in program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace adoNetTest
     {
         static void Main(string[] args)
         {
-            CRUD.Create();
+            string tableName, fullName, email, password, phoneNumber;
+            CRUD crud = new CRUD();
+
+            Console.Write("Table name >> ");
+            tableName = Console.ReadLine()!;
+            Console.Write("Fullname >> ");
+            fullName = Console.ReadLine()!;
+            Console.Write("Email >> ");
+            email = Console.ReadLine()!;
+            Console.Write("Password >> ");
+            password = Console.ReadLine()!;
+            Console.Write("Phone number >> ");
+            phoneNumber = Console.ReadLine()!;
+
+            Console.WriteLine(crud.Create(tableName, fullName, email, password, phoneNumber));
+
         }
 
     }
