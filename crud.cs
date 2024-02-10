ee9e40a699c6a1d7ecf5f8e94b75a9571e62c5bc
Nuriddin Asrorov <denrov13@gmail.com>
2024-02-10 22:47:40 +0500

    GetById solved

--- crud.cs
+++ crud.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace adoNetTest
 {
@@ -35,7 +36,30 @@ namespace adoNetTest
             NpgsqlConnection connection = new NpgsqlConnection(CONNECTIONSTRING);
             connection.Open();
 
-            query = $"select * from {tableName}";
+            query = $"select * from {tableName};";
+
+            NpgsqlCommand command = new NpgsqlCommand(query, connection);
+
+            NpgsqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.Write($"{reader[i]} ");
+                }
+                Console.WriteLine();
+            }
+
+            connection.Close();
+        }
+
+        public void GetById(int id)
+        {
+            NpgsqlConnection connection = new NpgsqlConnection(CONNECTIONSTRING);
+            connection.Open();
+
+            query = $"select * from {tableName} where Id = {id};";
 
             NpgsqlCommand command = new NpgsqlCommand(query, connection);
 
@@ -58,14 +82,31 @@ namespace adoNetTest
             NpgsqlConnection connection = new NpgsqlConnection(CONNECTIONSTRING);
             connection.Open();
 
-            query = $"Create table if not exists {tableName}(id serial, fullName varchar(50), email varchar(100), password varchar(100), phoneNumber varchar(50));";
+            query = $"Insert into {tableName}({columnName}) values ('{data}');";
 
             NpgsqlCommand command = new NpgsqlCommand(query, connection);
 
             command.ExecuteNonQuery();
 
             connection.Close();
-            return $"{tableName} was created successfully!";
+            return $"Inserted successfully!";
         }
+
+        public string InsertMany(string fullName, string email, string password, string phoneNumber)
+        {
+            NpgsqlConnection connection = new NpgsqlConnection(CONNECTIONSTRING);
+            connection.Open();
+
+            query = $"Insert into {tableName}(fullName, email, password, phoneNumber) values ('{fullName}', '{email}', '{password}', '{phoneNumber}');";
+
+            NpgsqlCommand command = new NpgsqlCommand(query, connection);
+
+            command.ExecuteNonQuery();
+
+            connection.Close();
+            return $"Inserted successfully!";
+        }
+
+
     }
 }
